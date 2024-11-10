08b62280310a7627bfed63a466f2e1db616e2e68
dimaProtas <dima_protasevich92@mail.ru>
2024-11-10 22:34:01 +0300

    Add project Go

--- main.go
+++ main.go
@@ -1,1 +1,162 @@
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+type Source struct {
+	ID   interface{} `json:"id"`
+	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+type Search struct {
+	SearchKey  string
+	NextPage   int
+	TotalPages int
+	Results    Results
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
+
+func (s *Search) IsLastPage() bool {
+	return s.NextPage > s.TotalPages
+}
+
+func (a *Article) FormatPublishetData() string {
+	year, moonth, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v-%d-%d", moonth, day, year)
+}
+
+func serchHendeler(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var API_KEY = os.Getenv("API_KEY")
+
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := u.Query()
+	searchKey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println(searchKey)
+	fmt.Println(page)
+
+	search := &Search{}
+	search.SearchKey = searchKey
+
+	next, err := strconv.Atoi(page)
+	if err != nil {
+		http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+		return
+	}
+
+	search.NextPage = next
+	pageSize := 2
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=ru", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, API_KEY)
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Посмотреть входящий json обьект
+	// body, err := io.ReadAll(resp.Body)
+	// fmt.Println("JSON response:", string(body))
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults) / float64(pageSize)))
+	fmt.Println(search.TotalPages, "TotalPages")
+	// добавьте этот if блок
+	if ok := !search.IsLastPage(); ok {
+		search.NextPage++
+		fmt.Println(search.NextPage)
+	}
+	err = tpl.Execute(w, search)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("assets/"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", serchHendeler)
+	http.ListenAndServe(":"+port, mux)
+	// fmt.Println(API_KEY)
+}
